b1889d2236ed90454b7e486c1f165540680990e6
zel <zeldanganteng@gmail.com>
2026-03-22 21:11:50 +0700

    update line sdk

--- index.js
+++ index.js
@@ -24,12 +24,21 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 })
 
 async function handleEvent(event) {
-  if (event.type === 'memberJoined') {
+if (event.type === 'memberJoined') {
     const members = event.joined.members
     for (const member of members) {
       await client.replyMessage(event.replyToken, {
-        type: 'text',
-        text: 'hi﹐ welcome to ﹒h͟i͟b͟i͟g͟o͟u͟ 🏄🏻‍♀️\n\nmake yourself at home, enjoy shopping!\n▸ invite temen harus pc admin!\n▸jangan hapus album, notes, atau kick member. or, you\'ll get 𝗯𝗮𝗻𝗻𝗲𝗱 :3\n\nplease read this ⤸ gohibigou.carrd.co'
+        type: 'textV2',
+        text: 'hi﹐ {user} welcome to ﹒h͟i͟b͟i͟g͟o͟u͟ 🏄🏻‍♀️\n\nmake yourself at home, enjoy shopping!\n▸ invite temen harus pc admin!\n▸jangan hapus album, notes, atau kick member. or, you\'ll get 𝗯𝗮𝗻𝗻𝗲𝗱 :3\n\nplease read this ⤸ gohibigou.carrd.co',
+        substitution: {
+          user: {
+            type: 'mention',
+            mentionee: {
+              type: 'user',
+              userId: member.userId
+            }
+          }
+        }
       })
     }
     return
