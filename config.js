ff40444a7260d0194211038077ca0700ced7e25d
zel <zeldanganteng@gmail.com>
2026-03-26 12:46:16 +0700

    like

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  ADMIN_IDS: [
-    'U10a4c4258d49d97ca76186aa00fd968e', 
-    'U4bdd3b3f391fcccd32e1e8475ef9015f']
+  ADMIN_IDS: ['U10a4c4258d49d97ca76186aa00fd968e', 'U4bdd3b3f391fcccd32e1e8475ef9015f']
 }
